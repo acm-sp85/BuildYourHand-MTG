285399cd32042cb4e27fb3e5c51bc4995326dc41
acm-sp85 <acontell@gmail.com>
2021-04-21 14:57:28 -0400

    testing MET API

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("linked")
+document.addEventListener('DOMContentLoaded', (event) => {
+
+    console.log("DOM loaded")
+
+    fetchMetData()
+    
+    
+    
+    
+})
+
+const fetchMetData = () => {
+    fetch("https://collectionapi.metmuseum.org/public/collection/v1/search?q=hopper")
+    .then((result) => result.json())
+    .then((data) => console.log(data.objectIDs))
+
+}
